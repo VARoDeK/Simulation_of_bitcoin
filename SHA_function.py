108b172e1368db3232ad73ef7c87e7a22447e2ce
Koder Joker <koderjoker@gmail.com>
2018-04-15 17:42:37 +0530

    Add SHA_function changes and .gitignore

--- SHA_function.py
+++ SHA_function.py
@@ -5,7 +5,7 @@ import sys
 
 homedir = os.environ['HOME']
 
-path = homedir + "/betacoin/sha/folder" + "/input.txt"
+path = homedir + "/betacoin/sha/" + "input.txt"
 try:
     f = open(path, "r")
 except FileNotFoundError:
@@ -13,13 +13,13 @@ except FileNotFoundError:
     sys.exit()
 
 a = f.read()
-a = a.encode(encoding="utf-8")
+a = a[:-1].encode(encoding="utf-8")
 f.close()
 
 m = hashlib.sha256()
 m.update(a)
 
-path = homedir + "/betacoin/sha/folder" + "/output.txt"
-f = open(path, "w+")
+path = homedir + "/betacoin/sha/" + "output.txt"
+f = open(path, "w")
 f.write(m.hexdigest())
 f.close()
