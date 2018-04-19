bb7cec86203eb12543a4dac840b2bd60ae3eb42d
Koder Joker <koderjoker@gmail.com>
2018-04-19 14:56:12 +0530

    Change import method

--- SHA_function.py
+++ SHA_function.py
@@ -1,22 +1,22 @@
-import hashlib
-import os
-import sys
+from hashlib import sha265
+from os import environ
+from sys import exit
 
 
-homedir = os.environ['HOME']
+homedir = environ['HOME']
 
 path = homedir + "/betacoin/sha/" + "input.txt"
 try:
     f = open(path, "r")
 except FileNotFoundError:
     print("File doesn't exist at destination")
-    sys.exit()
+    exit()
 
 a = f.read()
 a = a[:-1].encode(encoding="utf-8")
 f.close()
 
-m = hashlib.sha256()
+m = sha256()
 m.update(a)
 
 path = homedir + "/betacoin/sha/" + "output.txt"
