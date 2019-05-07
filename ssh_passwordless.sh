9c2cb5eab6e5f170060395ee43be3550c4dfe5d1
VARoDeK <vaibhavgupta40@gmail.com>
2019-05-07 15:46:56 +0530

    Add shell script for passwordless ssh

---
+++ ssh_passwordless.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter the username: " uname
+read -p "Enter hostname/ip: " host
+
+ssh-keygen -t rsa
+ssh $uname@$host mkdir -p .ssh
+cat .ssh/id_rsa.pub | ssh $uname@$host 'cat >> .ssh/authorized_keys'
+ssh $uname@$host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+
+echo "You can login now. Type: ssh $uname@$host "
