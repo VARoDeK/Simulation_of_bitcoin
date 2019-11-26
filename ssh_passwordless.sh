907350633684bfabeb66c350a9912ab4e99d239c
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2019-11-26 17:41:50 +0530

    correct path for passwordless

--- ssh_passwordless.sh
+++ ssh_passwordless.sh
@@ -4,7 +4,7 @@ read -p "Enter hostname/ip: " host
 
 ssh-keygen -t rsa
 ssh $uname@$host mkdir -p .ssh
-cat .ssh/id_rsa.pub | ssh $uname@$host 'cat >> .ssh/authorized_keys'
+cat ~/.ssh/id_rsa.pub | ssh $uname@$host 'cat >> .ssh/authorized_keys'
 ssh $uname@$host "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 
 echo "You can login now. Type: ssh $uname@$host "
